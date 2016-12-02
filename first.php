6eec6ccbb3f5196bcd590a6340fb666d12dbaec4
DmitriyYakovenko <dmitriyyakov@gmail.com>
2016-12-02 12:45:50 +0200

    this is commit

---
+++ first.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+  <?php  //echo "hello world<br>"; 
+//  phpinfo();
+//  $x = 32;
+//  $y = 22.13;
+// $summ = $x + $y;
+//  $sub = $x - $y;
+//  $mul = $x * $y;
+// $div = $x / $y;
+// /$modul = $x%$y;
+
+// echo $summ."<br>".$sub."<br>".$mul."<br>".$div;
+?>
+
+<?php 
+	// $str1 = "First string";
+	// $str2 = "Second \"string\" &ltbr&gt";
+	// echo $str1."<br>".$str2;
+
+ ?>
+ <?php 
+ $x = 12;
+ $y = 34;
+ $z = 4.25;
+
+ $bool_1 = $x != $z;
+ echo $bool_1."<br>";
+ $bool_2 = $x < $y;
+ echo $bool_2."<br>";
+  ?>
+</body>
+</html>
